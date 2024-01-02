38793d4db5babe5992ad1b81b4ffd06354bdaf81
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-01-02 09:07:48 -0800

    a

--- src/Compo/HomeSec.jsx
+++ src/Compo/HomeSec.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './HomeSec.scss'
 import { Link } from 'react-router-dom';
 import { SiMoneygram } from "react-icons/si";
@@ -11,7 +11,65 @@ import { MdOutlineDeveloperMode } from "react-icons/md";
 import { GoCodescanCheckmark } from "react-icons/go";
 import { MdOutlineSendTimeExtension } from "react-icons/md";
 
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
+
+
 const HomeSec = () => {
+    const [show, setShow] = useState(false)
+
+
+  const form = useRef();
+
+
+  
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [isValid, setIsValid] = useState(false);
+
+  const validatePhoneNumber = (number) => {
+    const isValidNumber = /^\d{11}$/.test(number);
+    setIsValid(isValidNumber);
+    return isValidNumber;
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    const newPhoneNumber = event.target.value;
+    setPhoneNumber(newPhoneNumber);
+    validatePhoneNumber(newPhoneNumber);
+  };
+
+
+
+
+  const sendEmail = (e) => {
+
+    if (validatePhoneNumber(phoneNumber)) {
+      console.log('Valid phone number:', phoneNumber);
+    } else {
+      console.log('Invalid phone number');
+    }
+
+
+    e.preventDefault();
+    
+
+    emailjs.sendForm('service_84n2o4x', 'template_bi2lptt', form.current, 'SePfbvFDjlAZc65wg')
+      .then((result) => {
+          console.log(result.text);
+          console.log('Email Successfully sent')
+          setShow(true)
+          e.target.reset()
+      }, (error) => {
+          console.log(error.text);
+          console.log('Email Failure')
+    });
+
+  };
   return (
     <div>
         <div className='headerSec'>
@@ -118,6 +176,71 @@ const HomeSec = () => {
                 </div>
 
             </div>
+        </div>
+
+
+        <div className='contDivMain'>
+        <div className='formDiv' data-aos="fade-up" data-aos-duration="3000">
+        <form action="" onSubmit={sendEmail} ref={form}>
+
+
+
+
+            <div className='inputDiv'>
+                <label htmlFor="">Name</label>
+                <input type="text" id='name' name='user_name' placeholder='Provide Name: ' required/>
+            </div>
+
+            <div className='inputDiv'>
+                <label htmlFor="">Number</label>
+                <input type="text" id='phone' name='user_number' placeholder='Provide Number: '
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}required/>
+                  {/* {!isValid && <p style={{ color: 'pink', paddingBottom : 10 }}>Field Must not be empty.</p>} */}
+                  {/* {isValid && <p style={{ color: 'green', paddingBottom : 10 }}></p>} */}
+            </div>
+
+
+            <div className='inputDiv'>
+                <label htmlFor="">Email</label>
+                <input 
+                  type="email" 
+                  id="email"
+                  name="user_email"
+                
+                placeholder='Provide Email: ' required/>
+
+            </div>
+
+            <div className='inputDiv'>
+              <label className='textLabel'>
+                Talk to us, about what you want, and we’ll respond as soon as you submit:
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder='Great, so what would you like us to do for you 😁' required>
+              </textarea>
+            </div>
+
+            <button type="submit">Book Now</button>
+          
+        </form>
+        
+      </div>
+
+
+        <div>
+          {show && (<div>
+            <div className='modal'>
+            <div className='modal-content'>
+              <h2>Message Sucessfull</h2>
+              <p>Message has been sent successfully, you will recieve a reply shortly</p>
+              <button onClick={()=>{setShow(false)}}>Close</button>
+            </div>
+          </div>
+          </div>)}
+        </div>
         </div>
     </div>
   )
