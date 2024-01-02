dd64e5558a0d5b5b0af7016fb0f9baa7cb5e12b2
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-01-02 08:54:28 -0800

    a

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Compo/Narvbar'
 import HomeSec from '../Compo/HomeSec'
+import Footer from '../Compo/Footer'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <HomeSec />
+        <Footer />
     </div>
   )
 }
