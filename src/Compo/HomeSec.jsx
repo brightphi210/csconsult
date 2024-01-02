4314cd7630bedbb1ac375d2cb113ae6fb659695e
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-01-02 07:28:52 -0800

    a

---
+++ src/Compo/HomeSec.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomeSec.scss'
+
+const HomeSec = () => {
+  return (
+    <div>
+        <div className='headerSec'>
+            <h2>
+                Take your company to new 
+                levels of success and productivity.
+            </h2>
+        </div>
+    </div>
+  )
+}
+
+export default HomeSec
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Compo/Narvbar'
+import HomeSec from '../Compo/HomeSec'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <HomeSec />
     </div>
   )
 }
